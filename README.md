dbb12b797b3cd84a97264024140bfc214aaf2f2c
Sydney <sydney@Sydneys-MacBook-Pro.local>
2022-10-21 11:35:27 -0700

    not working mint transaction

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # token-sender-contract
+
+### Minting Token
+# Next, run the command
+	$ cabal exec token-policy -- policy.plutus your_txHash#your_TxIX desired_number_of_tokens desired_token_name
+	$ cat policy.plutus  
+
+# Next, generate the policy.id for token. 
+	$ cardano-cli transaction policyid --script-file policy.plutus > policy.id
+
+# Add policy id into the token_meta.json, and change whatever else as desired
+
+# Mint With
+    $./scripts/mint-token-cli.sh your_txHash#your_TxIX desired_number_of_tokens desired_token_name   
+    (You will need to add your address file path and skey file path in the shell command file)
---
+++ scripts/mint-token-cli.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+oref=$1
+amt=$2
+tn=$3
+addrFile=wallets/epool.address
+skeyFile=wallets/epool-payment-0.skey
+
+echo "oref: $oref"
+echo "amt: $amt"
+echo "tn: $tn"
+echo "address file: $addrFile"
+echo "signing key file: $skeyFile"
+
+ppFile=pparams.json
+cardano-cli query protocol-parameters $MAGIC --out-file $ppFile
+
+policyFile=policy.plutus
+cabal exec token-policy $policyFile $oref $amt $tn
+
+unsignedFile=tx.unsigned
+signedFile=tx.signed
+pid=$(cardano-cli transaction policyid --script-file $policyFile)
+tnHex=$(cabal exec token-name -- $tn)
+addr=$(cat $addrFile)
+v="$amt $pid.$tnHex"
+
+echo "currency symbol: $pid"
+echo "token name (hex): $tnHex"
+echo "minted value: $v"
+echo "address: $addr"
+
+cardano-cli transaction build \
+    --babbage-era \
+    $MAGIC \
+    --tx-in $oref \
+    --tx-in-collateral $oref \
+    --required-signer $skeyFile \
+    --tx-out "$addr + 1500000 lovelace + $v" \
+    --mint "$v" \
+    --mint-script-file $policyFile \
+    --mint-redeemer-file unit.json \
+    --metadata-json-file token_meta.json \
+    --change-address $addr \
+    --protocol-params-file $ppFile \
+    --out-file $unsignedFile \
+
+
+
+cardano-cli transaction sign \
+    --tx-body-file $unsignedFile \
+    --signing-key-file $skeyFile \
+    $MAGIC \
+    --out-file $signedFile
+
+
+
+cardano-cli transaction submit \
+    $MAGIC \
+    --tx-file $signedFile
