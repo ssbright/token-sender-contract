9f2c71ff50ed4d546d3372d9ab48cea2c8c73e82
Sydney <sydney@Sydneys-MacBook-Pro.local>
2022-10-20 20:46:56 -0700

    not working minter

---
+++ src/Utils.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE GADTs            #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Utils
+    ( unsafeReadTxOutRef
+    , writeUnit, writeJSON
+    , writeMintingPolicy
+    , unsafeTokenNameToHex
+    , getCredentials
+    ) where
+
+import           Cardano.Api                 as API
+import           Cardano.Api.Shelley         (Address (..), PlutusScript (..))
+import           Cardano.Crypto.Hash.Class   (hashToBytes)
+import           Cardano.Ledger.Credential   as Ledger
+import           Cardano.Ledger.Crypto       (StandardCrypto)
+import           Cardano.Ledger.Hashes       (ScriptHash (..))
+import           Cardano.Ledger.Keys         (KeyHash (..))
+import           Codec.Serialise             (serialise)
+import           Data.Aeson                  (decode, encode)
+import qualified Data.ByteString.Char8       as BS8
+import qualified Data.ByteString.Lazy        as LBS
+import qualified Data.ByteString.Short       as SBS
+import           Data.Maybe                  (fromJust, fromMaybe)
+import           Data.String                 (IsString (..))
+import           Data.Text                   (pack)
+import           Plutus.V1.Ledger.Credential as Plutus
+import           Plutus.V1.Ledger.Crypto     as Plutus
+import           Plutus.V1.Ledger.Value      (TokenName (..))
+import           PlutusTx                    (Data (..))
+import qualified PlutusTx
+import           PlutusTx.Builtins           (toBuiltin)
+import           PlutusTx.Builtins.Internal  (BuiltinByteString (..))
+import qualified Ledger                      as Plutus
+import           Wallet.Emulator.Wallet      (WalletId (..), Wallet (..))
+import           Wallet.Types                (ContractInstanceId (..))
+
+
+unsafeReadTxOutRef :: String -> Plutus.TxOutRef
+unsafeReadTxOutRef s =
+  let
+    (x, _ : y) = span (/= '#') s
+  in
+    Plutus.TxOutRef
+        { Plutus.txOutRefId  = fromString x
+        , Plutus.txOutRefIdx = read y
+        }
+
+dataToScriptData :: Data -> ScriptData
+dataToScriptData (Constr n xs) = ScriptDataConstructor n $ dataToScriptData <$> xs
+dataToScriptData (Map xs)      = ScriptDataMap [(dataToScriptData x, dataToScriptData y) | (x, y) <- xs]
+dataToScriptData (List xs)     = ScriptDataList $ dataToScriptData <$> xs
+dataToScriptData (I n)         = ScriptDataNumber n
+dataToScriptData (B bs)        = ScriptDataBytes bs
+
+
+
+writeJSON :: PlutusTx.ToData a => FilePath -> a -> IO ()
+writeJSON file = LBS.writeFile file . encode . scriptDataToJson ScriptDataJsonDetailedSchema . dataToScriptData . PlutusTx.toData
+
+
+writeUnit :: IO ()
+writeUnit = writeJSON "testnet/unit.json" ()
+
+
+writeMintingPolicy :: FilePath -> Plutus.MintingPolicy -> IO (Either (FileError ()) ())
+writeMintingPolicy file = writeFileTextEnvelope @(PlutusScript PlutusScriptV1) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . Plutus.getMintingPolicy
+
+unsafeTokenNameToHex :: TokenName -> String
+unsafeTokenNameToHex = BS8.unpack . serialiseToRawBytesHex . fromJust . deserialiseFromRawBytes AsAssetName . getByteString . unTokenName
+  where
+    getByteString (BuiltinByteString bs) = bs
+
+getCredentials :: Plutus.Address -> Maybe (Plutus.PaymentPubKeyHash, Maybe Plutus.StakePubKeyHash)
+getCredentials (Plutus.Address x y) = case x of
+    ScriptCredential _   -> Nothing
+    PubKeyCredential pkh ->
+      let
+        ppkh = Plutus.PaymentPubKeyHash pkh
+      in
+        case y of
+            Nothing                        -> Just (ppkh, Nothing)
+            Just (Plutus.StakingPtr _ _ _) -> Nothing
+            Just (StakingHash h)           -> case h of
+                ScriptCredential _    -> Nothing
+                PubKeyCredential pkh' -> Just (ppkh, Just $ Plutus.StakePubKeyHash pkh')
